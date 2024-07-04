5117995f106faa32249aa5d553a5b2e23add4431
zhoutao <952636694@qq.com>
2024-07-04 21:25:15 +0800

    自定义指令

---
+++ src/views/num/one2.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="aaa">
+    <li v-for="item in 50">{{ item }}</li>
+  </ul>
+  <el-button @click="goTop">回到顶部</el-button>
+</template>
+
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+
+const goTop = () => {
+  const cb = () => {
+    requestAnimationFrame(cb)
+  }
+  requestAnimationFrame(cb)
+}
+
+
+
+// const goTop = () => {
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// }
+
+// let dom;
+
+// onMounted(() => {
+//   dom = document.querySelector(".aaa");
+//   dom.addEventListener('scroll', handleScroll);
+
+//   // 确保 dom 不为 null，即确保元素已存在
+//   if (dom) {
+//     console.log('DOM 已加载:', dom);
+//   }
+// });
+
+// onBeforeUnmount(() => {
+//   if (dom) {
+//     dom.removeEventListener('scroll', handleScroll);
+//   }
+// });
+
+// const handleScroll = () => {
+//   console.log('滚动事件触发');
+// }
+</script>
+<style scoped >
+
+</style>
--- src/views/num/three.vue
+++ src/views/num/three.vue
@@ -1,10 +1,85 @@
 <template>
-  <div>
-    3
+  <h2>按Tab键，快速跳转数字输入框</h2>
+  <h3>按上下箭头键，快速打分</h3>
+  <div v-for="item in scoreArr">
+    <el-input-number step-strictly ref="inputNumRefs" v-model="item['getScore']" :min="0" :max="item['score']"
+      :step="0.5" />
+    <br>
+    <br>
   </div>
+  <el-input v-model="num1"></el-input>
+  <el-input v-model="num2"></el-input>
 </template>
+
 <script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+const num1 = ref(1)
+const num2 = ref(2)
+const scoreArr = ref([
+  {
+    "qid": 1124663333216773,
+    "qNo": 33,
+    "score": 2,
+    "getScore": 1
+  },
+  {
+    "qid": 1124663333224966,
+    "qNo": 34,
+    "score": 2,
+    "getScore": 2
+  },
+  {
+    "qid": 1124663333233158,
+    "qNo": 35,
+    "score": 2,
+    "getScore": 2
+  },
+  {
+    "qid": 1124663333241350,
+    "qNo": 36,
+    "score": 2,
+    "getScore": 1.5
+  },
+  {
+    "qid": 1124663333249542,
+    "qNo": 37,
+    "score": 2,
+    "getScore": 2
+  }
+])
 
+// 数组收集多个示例引用
+const inputNumRefs = ref([])
+
+// 第一个获取焦点的索引
+let starIndex = 0
+
+// 调用数字输入框实例去获取焦点
+const setFirstFocus = (starIndex) => {
+  inputNumRefs.value[starIndex].focus()
+}
+
+// 监听键盘事件
+const handleKeyDown = (event) => {
+  if (event.key === 'Tab') {
+    event.preventDefault(); // 阻止默认的Tab行为
+    starIndex = starIndex + 1 // 索引加一，往下走
+    // 若是跳转到最后一项，则回到第一项
+    if (starIndex == scoreArr.value.length) starIndex = 0
+    // 再执行，去获取焦点
+    setFirstFocus(starIndex)
+  }
+};
+
+// 在组件挂载时添加事件监听
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown);
+  // 默认让第一个获取焦点
+  setFirstFocus(0)
+});
+
+// 组件卸载移除事件监听
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyDown);
+});
 </script>
-
-<style scoped lang="scss"></style>
