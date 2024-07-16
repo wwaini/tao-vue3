183705b6c4bd62c91ef2acee7444303e44082ef5
zhoutao <952636694@qq.com>
2024-07-16 10:59:01 +0800

    脚本切换vite.config.js的接口

--- program/nodeTab.js
+++ program/nodeTab.js
@@ -2,18 +2,6 @@ import fs from 'fs';
 // inquirer这个库来创建交互式的命令行界面
 import inquirer from 'inquirer';
 
-// 去找到target这一行代码
-function findTarget(file) {
-  let arr = file.split('\n')
-  let targetStr = ''
-  for (let i = 0; i < arr.length; i++) { // 通过循环找到对应行
-    if (arr[i].includes('target:')) {
-      targetStr = arr[i] // 赋值一下，找到了
-      break
-    }
-  }
-  return targetStr
-}
 // 命令行可选后端接口范围
 const targetList = [
   {
@@ -33,10 +21,22 @@ const targetList = [
     value: "\t\t\t\ttarget: 'http://55.55.55.55:5555',",
   },
 ]
+// 去找到target这一行代码
+function findTarget(file) {
+  let arr = file.split('\n')
+  let targetStr = ''
+  for (let i = 0; i < arr.length; i++) { // 通过循环找到对应行
+    if (arr[i].includes('target:')) {
+      targetStr = arr[i] // 赋值一下，找到了
+      break
+    }
+  }
+  return targetStr
+}
 // 选择方法
 async function selectServe() {
   try {
-    // 在命令行选择了
+    // 在命令行进行选择
     const choose = await inquirer.prompt([
       {
         type: 'list',
@@ -46,12 +46,13 @@ async function selectServe() {
         default: targetList[0].value
       }
     ])
-    // 读取了文件vue.config.js为string格式
+    // 读取了文件vite.config.js为string格式
     let file = fs.readFileSync('./vite.config.js', 'utf-8')
     // 找到target这一行
     let proxyTarget = findTarget(file)
     // 替换我们新选的后端服务地址
     const newFile = file.replace(proxyTarget, choose.serve)
+    // 重新写入vite.config.js
     fs.writeFileSync('./vite.config.js', newFile)
   } catch (error) {
     throw error
