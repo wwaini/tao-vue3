a815b844905910fb57ad2728b74096cc9cadc288
zhoutao <952636694@qq.com>
2024-07-31 21:51:15 +0800

    首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,14 @@
   </div>
 </template>
 <script setup>
+import axios from 'axios';
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  axios.get('http://localhost:9999/getPersonMsg').then(res => {
+    console.log(res);
+  })
+})
 </script>
 
 <style scoped lang="scss"></style>
