3617e709e6449a5eafb20104371c75177557bc41
zhoutao <952636694@qq.com>
2024-07-31 21:50:33 +0800

    二级菜单更改

--- src/layout/leftMenu.vue
+++ src/layout/leftMenu.vue
@@ -6,11 +6,15 @@
   <el-menu :default-active="route.path" class="el-menu-vertical-demo left-bg" @open="handleOpen" @close="handleClose"
     background-color="#ffffff" router :collapse="col.isCollapse" text-color="auto" active-text-color="auto">
     <el-menu-item index="/">
-      <el-icon><HomeFilled /></el-icon>
+      <el-icon>
+        <HomeFilled />
+      </el-icon>
       <span slot="title">Home</span>
     </el-menu-item>
     <el-menu-item v-for="(item, index) in routeArr" :key="item.name" :index="item.path">
-      <el-icon><component :is="item.meta.icon"></component></el-icon>
+      <el-icon>
+        <component :is="item.meta.icon"></component>
+      </el-icon>
       <span slot="title">{{ item.meta.title }}</span>
     </el-menu-item>
   </el-menu>
@@ -25,38 +29,45 @@ const col = useCollapseStore()
 const route = useRoute()
 const routeArr = reactive([
   {
-    name: 'one',
-    path: '/one',
+    name: 'requestAnimationFrame',
+    path: '/requestAnimationFrame',
     meta: {
-      title: '一叶',icon:'Promotion'
+      title: '定时器和requestAnimationFrame', icon: 'Promotion'
     }
   },
   {
-    name: 'two',
-    path: '/two',
+    name: 'directives',
+    path: '/directives',
     meta: {
-      title: '二叶',icon:'Promotion'
+      title: '自定义指令', icon: 'Promotion'
     }
   },
   {
-    name: 'three',
-    path: '/three',
+    name: 'tab-nums',
+    path: '/tab-nums',
     meta: {
-      title: '三叶',icon:'Promotion'
+      title: '按Tab键切换', icon: 'Promotion'
     }
   },
   {
-    name: 'four',
-    path: '/four',
+    name: 'Echart',
+    path: '/Echart',
     meta: {
-      title: '四叶',icon:'Promotion'
+      title: 'Echart', icon: 'Promotion'
     }
   },
   {
-    name: 'five',
-    path: '/five',
+    name: 'xgplayer',
+    path: '/xgplayer',
     meta: {
-      title: '五叶',icon:'Promotion'
+      title: 'xgplayer视频用法', icon: 'Promotion'
+    }
+  },
+  {
+    name: 'my-select',
+    path: '/my-select',
+    meta: {
+      title: '下拉框触底加载自定义指令', icon: 'Promotion'
     }
   },
 ])
@@ -65,7 +76,6 @@ const handleClose = (key, keyPath) => { }
 </script>
 
 <style scoped lang="scss">
-
 // 头像旋转
 @keyframes donut-spin {
   0% {
@@ -76,17 +86,20 @@ const handleClose = (key, keyPath) => { }
     transform: rotate(360deg);
   }
 }
+
 .title-center {
   padding: 15px 0 0 10px;
   width: calc(100% - 1px);
   // text-align: left;
-  display:flex;
+  display: flex;
   line-height: 30px;
 }
+
 .logoC:hover {
   cursor: pointer;
   animation-play-state: paused; //滚动动画暂停
 }
+
 .logoC {
   width: 30px;
   height: 30px;
@@ -95,15 +108,18 @@ const handleClose = (key, keyPath) => { }
   margin-right: 5px;
   animation: donut-spin 10s linear infinite;
 }
+
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   height: 100vh;
 }
+
 .el-menu-vertical-demo[data-v-8431c9bc]:not(.el-menu--collapse) {
   border: 0;
   height: 100vh;
 }
-.el-menu{
+
+.el-menu {
   height: 100vh;
 
 }
