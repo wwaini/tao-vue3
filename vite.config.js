e29ccfad9d0cd796753a90cc475ee1039a1664fa
zhoutao <952636694@qq.com>
2024-08-14 16:43:47 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "https://localhost://9999", // 后台接口
+        target: 'http://localhost:9999/',
         changeOrigin: true,
         secure: false, // 如果是https接口，需要配置这个参数
         // ws: true, //websocket支持
